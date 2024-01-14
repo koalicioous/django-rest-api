fe71c6aaa1f17022da5c9c0de184a11a19157e80
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2024-01-14 18:05:22 +0800

    setup: db race condition checker command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,11 @@
 """
 Django command to pause execution until database is available
 """
+import time
+
+from psycopg2 import OperationalError as PsyOperationalError
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -8,4 +13,14 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        pass
+        self.stdout.write("Waiting for database...")
+        db_ready = False
+        while db_ready is False:
+            try:
+                self.check(databases=["default"])
+                db_ready = True
+            except (PsyOperationalError, OperationalError):
+                self.stdout.write("Database unavailable, waiting 2 second...")
+                time.sleep(2)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
